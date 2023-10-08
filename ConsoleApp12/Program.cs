8f9ec125c155e80801af6e5e90ac4ade18fbdaca
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2023-10-08 20:16:32 +0300

    add program.cs

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    //Задание 1.
+            //Треугольник задан координатами своих вершин. Найти периметр и площадь треугольника.
+
+            //Console.WriteLine("Введите координату х первой точки ");    
+            //double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите координату у первой точки ");
+            //double y1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите координату х второй точки");
+            //double x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите координату у второй точки");
+            //double y2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите координату х третьей точки");
+            //double x3 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите координату у третьей точки");
+            //double y3 = Convert.ToDouble(Console.ReadLine());
+
+            //double dlina1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            //double dlina2= Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));  
+            //double dlina3= Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));  
+
+            //Console.WriteLine($"Периметр = {dlina1+dlina2+dlina3:f2}");
+
+            //double p = (dlina1 + dlina2 + dlina3) / 2;
+            //double S = Math.Sqrt(p * ((p - dlina1) * (p - dlina2) * (p - dlina3));  
+
+            //Console.WriteLine($"Площадь = {S:f2}");
+
+            //Задание 2
+            //Стоимость поездки на автомобиле (ввод: s – расстояние, b – расход бензина на 100 км, c - цена бензина за 1 литр).
+
+            //Console.Write("Введите расстояние = ");                
+            //double S = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите расход бензина на 100 км = ");
+            //double B = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите стоимость бензина = ");
+            //double C = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"Стоимость поездки составила {(S / 100) * B * C} руб.");
+
+            //Задание 3
+            //Известна цена 1 кг конфет, печенья и яблок. Найти стоимость всей покупки, если купили x кг конфет, у кг печенья и z кг яблок.
+
+            //Console.Write("Введите цену конфет = ");                 
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите цену печенья = ");
+            //double p = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите цену яблок = ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Введите X кг конфет = ");                 
+            //double X2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите Y кг печенья = ");
+            //double Y2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите Z кг яблок = ");
+            //double Z2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine($"Стоимость всей покупки {(c * X2) + (p * Y2) + (a * Z2)} руб.");
+
+            //Console.ReadKey();
+        }
+    }
+}
